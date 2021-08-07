9dc1c372dd58a3c74d2c945c2a22596feda410eb
Trí Trịnh <minhtrizs2000@gmail.com>
2021-08-07 22:31:23 +0700

    first create project

---
+++ movie/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <div className="container">
+                <div className="shadow-sm ...">
+                    trịnh
+                </div>
+                <div className="shadow ...">
+                    phan
+                </div>
+                <div className="shadow-md ...">
+                    minh
+                </div>
+                <div className="shadow-lg ...">
+                    trí
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default App;
